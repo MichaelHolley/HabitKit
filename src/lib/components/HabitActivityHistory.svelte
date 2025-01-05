022ff8679b5c993c1d7fa796b6b40873dc3c4149
Michael Holley <michael.philipp.holley@gmail.com>
2025-01-05 19:56:10 +0100

    Habit History component weekdays and months

--- src/lib/components/HabitActivityHistory.svelte
+++ src/lib/components/HabitActivityHistory.svelte
@@ -1,59 +1,87 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 
 	let { dates, showWeeks } = $props();
 	const activeDateSet = $derived(new Set(dates));
 
-	let weeks = $state([]);
+	const days: Date[] = $state([]);
 
-	let today = new Date();
-	let startDate = new Date(today);
-	startDate.setDate(today.getDate() - today.getDay() + 1); // Set to the first day of the current week (Monday)
-	startDate.setDate(startDate.getDate() - 7 * showWeeks); // Adjust to start from the past 51 weeks
+	const months = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec'
+	];
 
-	let endDate = new Date(today);
-	endDate.setDate(endDate.getDate() + (7 - endDate.getDay())); // Ensure endDate is a Sunday
+	const today = new Date();
+	const startDate = new Date(today);
+	// set the start date to the monday of showWeeks weeks ago from today
+	startDate.setDate(today.getDate() - today.getDay() + 1 - 7 * showWeeks);
 
 	onMount(() => {
 		let current = new Date(startDate);
-
-		while (current <= endDate) {
-			let week = [];
-			// Loop Monday to Sunday
-			for (let i = 0; i < 7; i++) {
-				week.push(new Date(current));
-				current.setDate(current.getDate() + 1);
-			}
-			weeks.push(week);
+		while (current <= today) {
+			current.setDate(current.getDate() + 1);
+			days.push(new Date(current));
 		}
 	});
 
-	// Function to format dates as 'YYYY-MM-DD'
-	const formatDate = (date) => date.toISOString().split('T')[0];
+	const formatDate = (date: Date) => date.toISOString().split('T')[0];
+
+	const getMonthChangeIndex = (dates: Date[]) => {
+		let monthChangeIndex = -1;
+		for (let i = 1; i < dates.length; i++) {
+			if (dates[i].getMonth() !== dates[i - 1].getMonth()) {
+				monthChangeIndex = i;
+				break;
+			}
+		}
+
+		return monthChangeIndex;
+	};
+
+	// Function which checks if one of the dates in the array within the first 7 days of the month
+	const isNewMonth = (dates: Date[]) => {
+		for (let i = 0; i < 7; i++) {
+			if (dates[i].getDate() <= 7) {
+				return true;
+			}
+		}
+
+		return false;
+	};
 </script>
 
-<div class="flex flex-col">
-	{#each { length: 7 } as _, i}
-		<div class="flex flex-row items-center gap-1 text-xs">
-			{#if i === 0}
-				<div class="min-w-8">Mon</div>
-			{:else if i == 2}
-				<div class="min-w-8">Wed</div>
-			{:else if i == 4}
-				<div class="min-w-8">Fri</div>
-			{:else if i == 6}
-				<div class="min-w-8">Sun</div>
-			{:else}
-				<div class="min-w-8"></div>
-			{/if}
-			{#each weeks as week}
-				<div
-					class="h-3 w-3 rounded-sm bg-neutral"
-					class:active={activeDateSet.has(formatDate(week[i]))}
-					title={formatDate(week[i])}
-				></div>
-			{/each}
-		</div>
+<div class="grid grid-flow-col grid-rows-8 items-center justify-center gap-1 text-[0.5rem]">
+	<div></div>
+	<div class="pr-1">Mon</div>
+	<div></div>
+	<div class="pr-1">Wed</div>
+	<div></div>
+	<div class="pr-1">Fri</div>
+	<div></div>
+	<div class="pr-1">Sun</div>
+	{#each days as day, i}
+		{#if i % 7 === 0 && day.getDate() <= 7}
+			<div class="-mr-2">
+				{months[days[i + 6].getMonth()]}
+			</div>
+		{:else if i % 7 === 0}
+			<div></div>
+		{/if}
+		<div
+			class="h-3 w-3 rounded-sm bg-neutral"
+			class:active={activeDateSet.has(formatDate(day))}
+			title={formatDate(day)}
+		></div>
 	{/each}
 </div>
 
