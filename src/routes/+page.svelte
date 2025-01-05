0bda8c6b6cf001b47d1f3eed9604c537c453615c
Michael Holley <michael.philipp.holley@gmail.com>
2025-01-05 17:32:43 +0100

    minor fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { goto } from '$app/navigation';
 	import HabitActivityHistory from '$lib/components/HabitActivityHistory.svelte';
 	import type { PageServerData } from './$types';
 
