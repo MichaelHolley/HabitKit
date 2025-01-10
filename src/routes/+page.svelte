0cfa4778a3e22583c8a1450b9b9b8f3aa8f5bd1f
Michael Holley <michael.philipp.holley@gmail.com>
2025-01-10 13:53:11 +0100

    date and font adjustment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,16 +26,16 @@
 	</div>
 
 	<div class="my-6">
-		<h3 class="mb-3 text-2xl">Last 30 Days</h3>
+		<h3 class="mb-3 text-xl">Last 30 Days</h3>
 		<div class="flex flex-shrink">
 			<div class="grid items-center gap-1 rounded-lg bg-base-200 p-4 text-[0.5rem]">
 				<div class="col-span-1"></div>
-				<div class="col-span-5">{thirtyDaysAgo.format('DD-MMM')}</div>
-				<div class="col-span-5">{thirtyDaysAgo.add(5, 'day').format('DD-MMM')}</div>
-				<div class="col-span-5">{thirtyDaysAgo.add(10, 'day').format('DD-MMM')}</div>
-				<div class="col-span-5">{thirtyDaysAgo.add(15, 'day').format('DD-MMM')}</div>
-				<div class="col-span-5">{thirtyDaysAgo.add(20, 'day').format('DD-MMM')}</div>
-				<div class="col-span-4">{thirtyDaysAgo.add(25, 'day').format('DD-MMM')}</div>
+				<div class="col-span-5">{thirtyDaysAgo.add(1, 'day').format('DD-MMM')}</div>
+				<div class="col-span-5">{thirtyDaysAgo.add(6, 'day').format('DD-MMM')}</div>
+				<div class="col-span-5">{thirtyDaysAgo.add(11, 'day').format('DD-MMM')}</div>
+				<div class="col-span-5">{thirtyDaysAgo.add(16, 'day').format('DD-MMM')}</div>
+				<div class="col-span-5">{thirtyDaysAgo.add(21, 'day').format('DD-MMM')}</div>
+				<div class="col-span-4">{thirtyDaysAgo.add(26, 'day').format('DD-MMM')}</div>
 				<div class="col-span-1">{thirtyDaysAgo.add(30, 'day').format('DD-MMM')}</div>
 
 				{#each data.summary as summaryItem}
@@ -55,15 +55,18 @@
 		</div>
 	</div>
 
-	<div class="flex flex-row flex-wrap justify-start gap-6">
-		{#each data.habits as habit}
-			<a href="/{habit.id}" class="flex flex-col gap-1">
-				<span class="link-hover link link-primary text-xl">{habit.name}</span>
-				<div class="rounded-lg bg-base-200 p-4">
-					<HabitActivityHistory dates={habit.dates} showWeeks={12} />
-				</div>
-			</a>
-		{/each}
+	<div class="my-6">
+		<h2 class="mb-3 text-2xl">Overview</h2>
+		<div class="flex flex-row flex-wrap justify-start gap-6">
+			{#each data.habits as habit}
+				<a href="/{habit.id}" class="flex flex-col gap-1">
+					<span class="link-hover link link-primary text-xl">{habit.name}</span>
+					<div class="rounded-lg bg-base-200 p-4">
+						<HabitActivityHistory dates={habit.dates} showWeeks={12} />
+					</div>
+				</a>
+			{/each}
+		</div>
 	</div>
 </section>
 
