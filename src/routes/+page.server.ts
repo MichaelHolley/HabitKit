cce932a398f22b81cb801e97ae19896e869dec87
Michael Holley <michael.philipp.holley@gmail.com>
2025-01-10 07:47:24 +0100

    addind dayjs and last30days activity

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,7 @@ import * as auth from '$lib/server/auth';
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 import { getUserHabits } from '$lib/server/habit';
+import { Prisma } from '@prisma/client';
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
@@ -10,7 +11,28 @@ export const load: PageServerLoad = async (event) => {
 
 	const habits = await getUserHabits(event.locals.user.id);
 
-	return { user: event.locals.user, habits: habits };
+	// create a const summary which is an array of objects with the habit dates of the last 30 days. dates have the format YYYY-MM-DD as string
+	const summary = habits.map((habit) => {
+		let dates = habit.dates as Prisma.JsonArray;
+		const today = new Date();
+
+		if (typeof dates === 'object' && Array.isArray(dates)) {
+			dates = dates.filter((date) => {
+				const dateAsDate = new Date(date as string);
+				return dateAsDate.getTime() > today.getTime() - 30 * 24 * 60 * 60 * 1000;
+			});
+		}
+
+		console.log(dates);
+
+		return {
+			id: habit.id,
+			name: habit.name,
+			dates: dates
+		};
+	});
+
+	return { user: event.locals.user, habits: habits, summary: summary };
 };
 
 export const actions: Actions = {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import ActivityBubble from '$lib/components/ActivityBubble.svelte';
 	import HabitActivityHistory from '$lib/components/HabitActivityHistory.svelte';
+	import dayjs from 'dayjs';
 	import type { PageServerData } from './$types';
 
 	let { data }: { data: PageServerData } = $props();
+	const thirtyDaysAgo = dayjs().subtract(30, 'days');
 </script>
 
 <div class="mb-7 flex flex-row items-center justify-between">
@@ -16,18 +19,55 @@
 	</div>
 </div>
 
-<div class="mb-3 flex flex-row items-center gap-6">
-	<h1 class="text-3xl">Habits</h1>
-	<a href="/create" class="btn btn-outline btn-primary btn-sm">+ Track New</a>
-</div>
+<section class="my-6">
+	<div class="flex flex-row items-center gap-6">
+		<h2 class="text-3xl">Habits</h2>
+		<a href="/create" class="btn btn-outline btn-primary btn-sm">+ Track New</a>
+	</div>
 
-<div class="flex flex-row flex-wrap justify-start gap-6">
-	{#each data.habits as habit}
-		<a href="/{habit.id}" class="flex flex-col gap-1">
-			<span class="link-hover link link-primary text-xl">{habit.name}</span>
-			<div class="rounded-lg bg-base-200 p-4">
-				<HabitActivityHistory dates={habit.dates} showWeeks={12} />
-			</div>
-		</a>
-	{/each}
-</div>
+	<div class="my-6 max-w-2xl">
+		<h3 class="mb-3 text-2xl">Last 30 Days</h3>
+		<div class="grid items-center gap-1 rounded-lg bg-base-200 p-4 text-[0.5rem]">
+			<div class="col-span-1"></div>
+			<div class="col-span-5">{thirtyDaysAgo.format('DD-MMM')}</div>
+			<div class="col-span-5">{thirtyDaysAgo.add(5, 'day').format('DD-MMM')}</div>
+			<div class="col-span-5">{thirtyDaysAgo.add(10, 'day').format('DD-MMM')}</div>
+			<div class="col-span-5">{thirtyDaysAgo.add(15, 'day').format('DD-MMM')}</div>
+			<div class="col-span-5">{thirtyDaysAgo.add(20, 'day').format('DD-MMM')}</div>
+			<div class="col-span-4">{thirtyDaysAgo.add(25, 'day').format('DD-MMM')}</div>
+			<div class="col-span-1">{thirtyDaysAgo.add(30, 'day').format('DD-MMM')}</div>
+
+			{#each data.summary as summaryItem}
+				<a class="link-hover link link-primary text-sm" href="/{summaryItem.id}"
+					>{summaryItem.name}</a
+				>
+				{#each { length: 30 } as _, i}
+					<ActivityBubble
+						active={summaryItem.dates.includes(
+							thirtyDaysAgo.add(i + 1, 'day').format('YYYY-MM-DD')
+						)}
+						title={thirtyDaysAgo.add(i + 1, 'day').format('YYYY-MM-DD')}
+					/>
+				{/each}
+			{/each}
+		</div>
+	</div>
+
+	<div class="flex flex-row flex-wrap justify-start gap-6">
+		{#each data.habits as habit}
+			<a href="/{habit.id}" class="flex flex-col gap-1">
+				<span class="link-hover link link-primary text-xl">{habit.name}</span>
+				<div class="rounded-lg bg-base-200 p-4">
+					<HabitActivityHistory dates={habit.dates} showWeeks={12} />
+				</div>
+			</a>
+		{/each}
+	</div>
+</section>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(31, auto);
+	}
+</style>
