cca7f17f963b5a8ea2dbbf40813b3b2108c585d4
Michael Holley <michael.philipp.holley@gmail.com>
2025-01-24 18:58:37 +0100

    minor values table improvements

--- src/routes/(app)/[id]/(habit-subpages)/values/+page.svelte
+++ src/routes/(app)/[id]/(habit-subpages)/values/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import dayjs from 'dayjs';
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
@@ -7,17 +8,18 @@
 
 <h2 class="mb-3 text-3xl">{data.habit?.title}</h2>
 <div class="max-w-lg overflow-x-auto">
-	<table class="table table-xs">
-		<thead>
-			<tr>
-				<th>Date</th>
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each data.habit.dates as date}
+	<table class="table table-pin-rows table-xs">
+		{#each data.habit.dates as date, i}
+			{#if i === 0 || !dayjs(date as string).isSame(dayjs(data.habit.dates[i + 1] as string), 'month')}
+				<thead>
+					<tr>
+						<th colspan="2" class="text-neutral-500">{dayjs(date as string).format('MMM YYYY')}</th>
+					</tr>
+				</thead>
+			{/if}
+			<tbody>
 				<tr>
-					<td>{date}</td>
+					<td class="pl-4">{dayjs(date as string).format('DD MMM YYYY')}</td>
 					<td>
 						<form method="POST" action="?/delete" use:enhance>
 							<input type="hidden" name="date" value={date} />
@@ -25,7 +27,7 @@
 						</form>
 					</td>
 				</tr>
-			{/each}
-		</tbody>
+			</tbody>
+		{/each}
 	</table>
 </div>
