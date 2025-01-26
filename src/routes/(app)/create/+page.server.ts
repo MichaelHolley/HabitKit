6d67d1945719104fe134cc80dfd8bc9b90267de0
Michael Holley <michael.philipp.holley@gmail.com>
2025-01-26 13:45:41 +0100

    create presets impl

--- src/routes/(app)/create/+page.server.ts
+++ src/routes/(app)/create/+page.server.ts
@@ -1,6 +1,31 @@
-import { redirect } from '@sveltejs/kit';
-import type { Actions } from './$types';
 import { createHabit } from '$lib/server/habit';
+import { redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+	return {
+		presetOptions: [
+			{ title: 'Workout', description: 'Do at least 15 minutes of exercise a day' },
+			{ title: 'Meditation', description: '5 Minutes of deep breathing' },
+			{ title: 'Reading', description: 'Read at least 10 pages a day' },
+			{ title: '10K Steps', description: 'Achieve 10K Steps a day' },
+			{ title: 'Water', description: 'Drink at least 8 cups of water a day' },
+			{ title: 'Sleep', description: 'Get 7 hours of sleep a night' },
+			{
+				title: 'Plan Your Day',
+				description: 'Write down your priorities or goals for the day'
+			},
+			{
+				title: 'Get Outside',
+				description: 'Spend at least 15 minutes outdoors or in nature'
+			},
+			{
+				title: 'Journaling',
+				description: 'Write about your thoughts, ideas, or experiences'
+			}
+		]
+	};
+};
 
 export const actions: Actions = {
 	createHabit: async (event) => {
--- src/routes/(app)/create/+page.svelte
+++ src/routes/(app)/create/+page.svelte
@@ -1,22 +1,74 @@
-<script>
+<script lang="ts">
 	import { enhance } from '$app/forms';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+	let title = $state('');
+	let description = $state('');
+	let presetModal: HTMLDialogElement;
+
+	function selectPreset(preset: { title: string; description?: string }) {
+		title = preset.title;
+
+		if (preset.description) {
+			description = preset.description;
+		}
+
+		presetModal.close();
+	}
 </script>
 
 <div class="max-w-md">
-	<h3 class="pb-4 text-3xl">Create Habit</h3>
+	<div class="mb-4 flex flex-row items-end justify-between gap-3">
+		<h3 class="text-3xl">Create Habit</h3>
+		<p>
+			or <button
+				class="link link-primary"
+				onclick={() => {
+					presetModal.showModal();
+				}}>use a Preset</button
+			>
+		</p>
+	</div>
 	<form method="POST" action="?/createHabit" use:enhance class="flex flex-col items-end gap-3">
 		<label class="form-control w-full text-sm">
 			<div class="label">
 				<span class="label-text">Title</span>
 			</div>
-			<input name="title" class="input input-bordered" required />
+			<input name="title" class="input input-bordered" bind:value={title} required />
 		</label>
 		<label class="form-control w-full text-sm">
 			<div class="label">
 				<span class="label-text">Description (optional)</span>
 			</div>
-			<input name="description" class="input input-bordered" />
+			<input name="description" class="input input-bordered" bind:value={description} />
 		</label>
 		<button class="btn btn-primary w-fit">+ Create</button>
 	</form>
 </div>
+
+<dialog id="preset_modal" bind:this={presetModal} class="modal modal-bottom sm:modal-middle">
+	<div class="modal-box">
+		<form method="dialog">
+			<button class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">✕</button>
+		</form>
+		<h3 class="text-lg font-bold">Select a Preset</h3>
+		<p class="mb-3 py-1">Choose a preset to get started quickly and customize later.</p>
+		<div class="-mr-4 flex max-h-80 flex-grow flex-col gap-1 self-stretch overflow-y-scroll pr-4">
+			{#each data.presetOptions as preset}
+				<button
+					class="rounded-md border border-base-300 bg-base-200 px-4 py-2 text-left hover:bg-base-300"
+					onclick={() => selectPreset(preset)}
+				>
+					<div>{preset.title}</div>
+					{#if preset.description}
+						<div class="text-xs text-neutral-400">{preset.description}</div>
+					{/if}
+				</button>
+			{/each}
+		</div>
+	</div>
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
