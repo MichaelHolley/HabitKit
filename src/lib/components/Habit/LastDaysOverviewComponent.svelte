8cacf37cd5eb7c6468d77463414231c5060fa8bf
Michael Holley <michael.philipp.holley@gmail.com>
2025-01-30 19:10:08 +0100

    wip

--- src/lib/components/Habit/LastDaysOverviewComponent.svelte
+++ src/lib/components/Habit/LastDaysOverviewComponent.svelte
@@ -5,32 +5,39 @@
 	let { habits } = $props();
 
 	let containerWidth = $state(0);
-	let visibleDays = $derived(Math.ceil((containerWidth - 92) / 16) - 1);
+	let visibleDays = $derived(Math.ceil(containerWidth / 16) - 1);
 
 	const today = dayjs();
 </script>
 
 <div class="rounded-lg bg-base-200 p-4 pt-3">
-	<div bind:clientWidth={containerWidth}>
-		<h4 class="mb-1 text-right text-xs">Last {visibleDays} Days</h4>
-		<div
-			class="grid items-center gap-1 text-2xs"
-			style="grid-template-columns: 1fr repeat({visibleDays}, auto) !important;"
-		>
+	<h4 class="mb-1 text-right text-xs">Last {visibleDays} Days</h4>
+	<div class="flex flex-row justify-between gap-3">
+		<div class="flex flex-col justify-between">
 			{#each habits as habit}
 				<a
-					class="link-hover link link-primary sticky left-0 z-10 mr-2 bg-base-200 pr-1 text-sm"
+					class="link-hover link link-primary sticky left-0 z-10 bg-base-200 text-sm"
 					href="/{habit.id}">{habit.title}</a
 				>
-				{#each { length: visibleDays } as _, i}
-					<ActivityBubble
-						active={habit.dates.includes(
-							today.subtract(visibleDays - i - 1, 'days').format('YYYY-MM-DD')
-						)}
-						title={today.subtract(visibleDays - i - 1, 'days').format('YYYY-MM-DD')}
-					/>
-				{/each}
 			{/each}
+		</div>
+
+		<div class="my-1 flex flex-grow flex-col">
+			<div bind:clientWidth={containerWidth} class="flex h-full flex-col justify-between">
+				{#each habits as habit}
+					<div class="flex flex-row justify-end gap-1">
+						{#each { length: visibleDays } as _, i}
+							<ActivityBubble
+								active={habit.dates.includes(
+									today.subtract(visibleDays - i - 1, 'days').format('YYYY-MM-DD')
+								)}
+								title={today.subtract(visibleDays - i - 1, 'days').format('ddd YYYY-MM-DD')}
+								delay={i * 10}
+							/>
+						{/each}
+					</div>
+				{/each}
+			</div>
 		</div>
 	</div>
 </div>
