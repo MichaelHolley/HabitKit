52739b4e53f6736df488b7b8210ad1b84f7e6bd1
Michael Holley <michael.philipp.holley@gmail.com>
2025-03-06 20:42:26 +0100

    moved goals to bottom

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -11,19 +11,6 @@
 
 <section class="my-6">
 	<div class="flex flex-row items-center justify-between gap-4 xs:justify-start">
-		<h2 class="text-3xl">Goals</h2>
-		<a href="/goal/create" class="btn btn-outline btn-primary btn-sm">+ Add</a>
-	</div>
-
-	<div class="goal-overview-grid my-3 grid flex-wrap justify-start gap-3">
-		{#each visibleGoals as goal}
-			<GoalCardComponent {goal} />
-		{/each}
-	</div>
-</section>
-
-<section class="my-6">
-	<div class="flex flex-row items-center justify-between gap-4 xs:justify-start">
 		<h2 class="text-3xl">Habits</h2>
 		<a href="/create" class="btn btn-outline btn-primary btn-sm">+ Track New</a>
 	</div>
@@ -40,6 +27,19 @@
 				<HabitOverviewItem {habit} />
 			{/each}
 		</div>
+	</div>
+</section>
+
+<section class="my-6">
+	<div class="flex flex-row items-center justify-between gap-4 xs:justify-start">
+		<h2 class="text-3xl">Goals</h2>
+		<a href="/goal/create" class="btn btn-outline btn-primary btn-sm">+ Add</a>
+	</div>
+
+	<div class="goal-overview-grid my-3 grid flex-wrap justify-start gap-3">
+		{#each visibleGoals as goal}
+			<GoalCardComponent {goal} />
+		{/each}
 	</div>
 </section>
 
