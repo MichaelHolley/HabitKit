1b1b7738556686a7e7a04d453ef813734a92ebf7
Michael Holley <michael.philipp.holley@gmail.com>
2025-03-07 15:24:11 +0100

    wip

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -16,7 +16,7 @@
 	</div>
 
 	{#if data.habits.length > 0}
-		<div class="my-3 max-w-lg">
+		<div class="my-3 mt-2 max-w-lg">
 			<LastXDays habits={data.habits} />
 		</div>
 	{/if}
@@ -30,13 +30,13 @@
 	</div>
 </section>
 
-<section class="my-6 mt-10">
+<section class="my-6 mt-8">
 	<div class="flex flex-row items-center justify-between gap-4 xs:justify-start">
 		<h2 class="text-3xl font-extralight tracking-widest">Goals</h2>
 		<a href="/goal/create" class="btn btn-outline btn-primary btn-sm">+ Add</a>
 	</div>
 
-	<div class="goal-overview-grid my-3 grid flex-wrap justify-start gap-3">
+	<div class="goal-overview-grid my-3 mt-2 grid flex-wrap justify-start gap-3">
 		{#each visibleGoals as goal}
 			<GoalCardComponent {goal} />
 		{/each}
