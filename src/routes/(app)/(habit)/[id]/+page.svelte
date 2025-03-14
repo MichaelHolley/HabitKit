a6b2e172ffebf7ee4a458d12505734f6b163205d
Michael Holley <michael.philipp.holley@gmail.com>
2025-03-14 07:15:02 +0100

    minor fix

--- src/routes/(app)/(habit)/[id]/+page.svelte
+++ src/routes/(app)/(habit)/[id]/+page.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div>
-	<NavigateBackButton backUrl="/" />
+	<div class="mb-3"><NavigateBackButton backUrl="/" /></div>
 	<div class="mb-1 grid grid-cols-2">
 		<div>
 			<h2 class="text-3xl">{data.habit?.title}</h2>
@@ -28,7 +28,7 @@
 	<p class="text-xs text-neutral-400">{data.habit?.description}</p>
 </div>
 
-<div class="mb-4 mt-6 flex flex-row flex-wrap justify-between gap-2">
+<div class="mt-6 mb-4 flex flex-row flex-wrap justify-between gap-2">
 	<div class="flex flex-row gap-2">
 		<form method="POST" action="?/addToday" use:enhance class="flex flex-col justify-start">
 			<button
@@ -60,22 +60,11 @@
 <HabitHistory dates={data.habit?.dates} />
 
 <div class="my-6 flex flex-row flex-wrap items-center justify-end gap-8">
-	<form
-		method="POST"
-		action="?/addDate"
-		use:enhance
-		class="flex max-w-full grow flex-col items-end gap-3 sm:max-w-md"
-	>
-		<label class="form-control w-full text-sm">
-			<input
-				name="date"
-				type="date"
-				class="input input-bordered"
-				placeholder="Select a date"
-				required
-			/>
+	<form method="POST" action="?/addDate" use:enhance class="flex flex-col items-end gap-2">
+		<label class="input w-full">
+			<span>Date</span>
+			<input type="date" name="date" required placeholder="Select a date" />
 		</label>
-
 		<button class="btn btn-primary w-fit gap-1">
 			<Icon icon={ICON_MAP.plus} class="text-lg" />
 			Add Date
@@ -90,7 +79,7 @@
 <dialog id="delete_modal" bind:this={deleteModal} class="modal modal-bottom sm:modal-middle">
 	<div class="modal-box">
 		<form method="dialog">
-			<button class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">✕</button>
+			<button class="btn btn-circle btn-ghost btn-sm absolute top-2 right-2">✕</button>
 		</form>
 		<h3 class="text-lg font-bold">Confirmation</h3>
 		<p class="py-4">
