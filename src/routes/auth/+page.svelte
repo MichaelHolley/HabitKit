28ecd16a65fdfd5a3c4139052b37ff989a8b813c
Michael Holley <michael.philipp.holley@gmail.com>
2025-03-29 17:36:19 +0100

    wip

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -22,7 +22,7 @@
 
 <div class="flex h-screen flex-col items-center justify-center gap-8">
 	<div class="text-center">
-		<h1 class="text-primary pb-2 text-2xl">Welcome to Ascent</h1>
+		<h1 class="text-secondary pb-2 text-2xl">Welcome to Ascent</h1>
 		<p>Keep track of your habits and goals</p>
 	</div>
 	<form method="POST" action="?/login" use:enhance={handleLogin} class="flex flex-col gap-3">
@@ -35,7 +35,7 @@
 			required
 		/>
 		<button class="btn btn-primary">Login</button>
-		<button class="btn btn-outline btn-primary" formaction="?/register">Register</button>
+		<button class="btn btn-outline btn-secondary" formaction="?/register">Register</button>
 	</form>
 	<p style="color: red">{form?.message ?? ''}</p>
 </div>
