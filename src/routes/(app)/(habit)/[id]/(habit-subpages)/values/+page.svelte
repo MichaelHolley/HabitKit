4af5ad975106b68c2573b7eadbc708de041ce4ad
Michael Holley <michael.philipp.holley@gmail.com>
2025-06-26 16:38:41 +0200

    minor fix

--- src/routes/(app)/(habit)/[id]/(habit-subpages)/values/+page.svelte
+++ src/routes/(app)/(habit)/[id]/(habit-subpages)/values/+page.svelte
@@ -14,7 +14,7 @@
 			{#if i === 0 || !dayjs(date as string).isSame(dayjs(data.habit.dates[i - 1] as string), 'month')}
 				<thead>
 					<tr>
-						<th colspan="2" class="pt-4 text-neutral-500 dark:text-neutral-300">
+						<th colspan="2" class="text-base-content-300 pt-4">
 							{dayjs(date as string).format('MMM YYYY')}
 						</th>
 					</tr>
@@ -22,7 +22,7 @@
 			{/if}
 			<tbody>
 				<tr>
-					<td class="pl-4">{dayjs(date as string).format('ddd, DD MMM YYYY')}</td>
+					<td class="pl-4">{dayjs(date as string).format('DD MMM YYYY (ddd)')}</td>
 					<td>
 						<form method="POST" action="?/delete" use:enhance={defaultHandleDeleteSubmit}>
 							<input type="hidden" name="date" value={date} />
